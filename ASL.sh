3c07fdd3f284a3cde7a444395f7d94f277598845
prasang7 <prasang.sharma7@gmail.com>
2017-01-20 16:38:56 +0530

    First Commit

---
+++ ASL.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Created for fun!
+
+echo "."
+echo "ASL: Android Studio Launcher"
+echo "Created by prasang7"
+echo "."
+echo "Please Wait, Launching..."
+echo "."
+cd ..
+cd ..
+cd usr
+cd local
+cd android-studio
+cd bin
+./studio.sh
